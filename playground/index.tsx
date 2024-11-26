a3de04c897ffb90efe3bbb28bb3d8be51bd1b409
David Inoa <davidinoa@gmail.com>
2024-11-26 14:08:17 -0400

    complete problem 4.2

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import VanillaTilt from 'vanilla-tilt'
 
@@ -6,23 +6,36 @@ interface HTMLVanillaTiltElement extends HTMLDivElement {
 	vanillaTilt?: VanillaTilt
 }
 
-const vanillaTiltOptions = {
-	max: 25,
-	speed: 400,
-	glare: true,
-	'max-glare': 0.5,
-}
+function Tilt({
+	children,
+	max = 25,
+	speed = 400,
+	glare = true,
+	maxGlare = 0.5,
+}: {
+	children: React.ReactNode
+	max?: number
+	speed?: number
+	glare?: boolean
+	maxGlare?: number
+}) {
+	const tiltRef = useRef<HTMLVanillaTiltElement | null>(null)
 
-function Tilt({ children }: { children: React.ReactNode }) {
+	const vanillaTiltOptions = {
+		max,
+		speed,
+		glare,
+		'max-glare': maxGlare,
+	}
+
+	useEffect(() => {
+		if (!tiltRef.current) return
+		VanillaTilt.init(tiltRef.current, vanillaTiltOptions)
+		return () => tiltRef.current?.vanillaTilt?.destroy()
+	}, [vanillaTiltOptions])
+
 	return (
-		<div
-			className="tilt-root"
-			ref={(tiltNode: HTMLVanillaTiltElement | null) => {
-				if (!tiltNode) return
-				VanillaTilt.init(tiltNode, vanillaTiltOptions)
-				return () => tiltNode.vanillaTilt?.destroy()
-			}}
-		>
+		<div className="tilt-root" ref={tiltRef}>
 			<div className="tilt-child">{children}</div>
 		</div>
 	)
@@ -31,20 +44,65 @@ function Tilt({ children }: { children: React.ReactNode }) {
 function App() {
 	const [showTilt, setShowTilt] = useState(true)
 	const [count, setCount] = useState(0)
+	const [options, setOptions] = useState({
+		max: 25,
+		speed: 400,
+		glare: true,
+		maxGlare: 0.5,
+	})
 	return (
 		<div>
 			<button onClick={() => setShowTilt(s => !s)}>Toggle Visibility</button>
 			{showTilt ? (
-				<Tilt>
-					<div className="totally-centered">
-						<button
-							className="count-button"
-							onClick={() => setCount(c => c + 1)}
-						>
-							{count}
-						</button>
-					</div>
-				</Tilt>
+				<div className="app">
+					<form
+						onSubmit={e => e.preventDefault()}
+						onChange={event => {
+							const formData = new FormData(event.currentTarget)
+							setOptions({
+								max: Number(formData.get('max')),
+								speed: Number(formData.get('speed')),
+								glare: formData.get('glare') === 'on',
+								maxGlare: Number(formData.get('maxGlare')),
+							})
+						}}
+					>
+						<div>
+							<label htmlFor="max">Max:</label>
+							<input id="max" name="max" type="number" defaultValue={25} />
+						</div>
+						<div>
+							<label htmlFor="speed">Speed:</label>
+							<input id="speed" name="speed" type="number" defaultValue={400} />
+						</div>
+						<div>
+							<label>
+								<input id="glare" name="glare" type="checkbox" defaultChecked />
+								Glare
+							</label>
+						</div>
+						<div>
+							<label htmlFor="maxGlare">Max Glare:</label>
+							<input
+								id="maxGlare"
+								name="maxGlare"
+								type="number"
+								defaultValue={0.5}
+							/>
+						</div>
+					</form>
+					<br />
+					<Tilt {...options}>
+						<div className="totally-centered">
+							<button
+								className="count-button"
+								onClick={() => setCount(c => c + 1)}
+							>
+								{count}
+							</button>
+						</div>
+					</Tilt>
+				</div>
 			) : null}
 		</div>
 	)
@@ -53,3 +111,10 @@ function App() {
 const rootEl = document.createElement('div')
 document.body.append(rootEl)
 createRoot(rootEl).render(<App />)
+
+// 🤫 we'll fix this in the next step!
+// (ALMOST) NEVER DISABLE THIS LINT RULE IN REAL LIFE!
+/*
+eslint
+	react-hooks/exhaustive-deps: "off",
+*/
